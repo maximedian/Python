b49c2c034c9451eba3c5d052e9a857d1c8371a9c
maximedian <131916514+maximedian@users.noreply.github.com>
2023-04-26 21:35:50 +0100

    Create blornkszapat.py
    Introducing the very useful blornkszapat fuction, in python

---
+++ blornkszapat.py
@@ -0,0 +1,5 @@
+# Explanation: This python code returns the blornkszapat value given two input numbers.
+
+def blornkszapat(a, b):
+  # do stuff, apart from the blornkszapat function. Sprinkle some wallabywinter on it
+  return a + b + 2*a^2 - 3*b^2
